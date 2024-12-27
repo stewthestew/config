21cf287d1e3b639a70ab30510561c071d73eb654
stewthestew <stew156@proton.me>
2024-12-27 16:28:04 +0000

    GHOSTTY!!!

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias v='nvim'
+alias i='sudo pacman -S'
+alias r='sudo pacman -R'
+function fish_greeting
+end
+
+fish_add_path /home/noname/.spicetify
