bd77c78fe613d07fe9cd94d460c791bd29658aab
stewthestew <stew156@proton.me>
2025-01-03 20:11:22 +0000

    removed none-ls and removed the duplicate neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n",     "ErrorMsg" },
-            { out,                                "WarningMsg" },
-            { "\nand you fucking messed up again" },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nand you fucking messed up again" },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -18,198 +18,182 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 require("lazy").setup({
-    spec = {
+	spec = {
 
-        {
-            "zenbones-theme/zenbones.nvim",
-            dependencies = "rktjmp/lush.nvim",
-            lazy = false,
-            priority = 1000,
-        },
-        {
-            "MeanderingProgrammer/render-markdown.nvim",
-            dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-            ---@module 'render-markdown'
-            ---@type render.md.UserConfig
-            opts = {},
-        },
+		{
+			"zenbones-theme/zenbones.nvim",
+			dependencies = "rktjmp/lush.nvim",
+			lazy = false,
+			priority = 1000,
+		},
+		{
+			"MeanderingProgrammer/render-markdown.nvim",
+			dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+			---@module 'render-markdown'
+			---@type render.md.UserConfig
+			opts = {},
+		},
 
-        {
-            "supermaven-inc/supermaven-nvim",
-            config = function()
-                require("supermaven-nvim").setup({})
-            end,
-        },
-        {
-            "ggandor/leap.nvim",
-            dependencies = { "tpope/vim-repeat" },
-        },
-        {
-            "nvim-telescope/telescope.nvim",
-            tag = "0.1.8",
-            -- or                              , branch = '0.1.x',
-            dependencies = { "nvim-lua/plenary.nvim" },
-        },
+		{
+			"supermaven-inc/supermaven-nvim",
+			config = function()
+				require("supermaven-nvim").setup({})
+			end,
+		},
+		{
+			"ggandor/leap.nvim",
+			dependencies = { "tpope/vim-repeat" },
+		},
+		{
+			"nvim-telescope/telescope.nvim",
+			tag = "0.1.8",
+			-- or                              , branch = '0.1.x',
+			dependencies = { "nvim-lua/plenary.nvim" },
+		},
 
-        { "norcalli/nvim-colorizer.lua" },
+		{
+			"folke/zen-mode.nvim",
+			opts = {},
+		},
 
-        {
-            "folke/zen-mode.nvim",
-            opts = {},
-        },
+		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
-        { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+		{
+			"williamboman/mason.nvim",
+			config = function()
+				require("mason").setup()
+			end,
+		},
+		{
+			"williamboman/mason-lspconfig.nvim",
+			config = function()
+				require("mason-lspconfig").setup({
+					ensure_installed = { "lua_ls", "gopls" },
+				})
+			end,
+		},
 
-        {
-            "williamboman/mason.nvim",
-            config = function()
-                require("mason").setup()
-            end,
-        },
-        {
-            "williamboman/mason-lspconfig.nvim",
-            config = function()
-                require("mason-lspconfig").setup({
-                    ensure_installed = { "lua_ls", "gopls" },
-                })
-            end,
-        },
+		{
+			"neovim/nvim-lspconfig",
+			config = function()
+				local capabilities = require("cmp_nvim_lsp").default_capabilities()
+				local lspconfig = require("lspconfig")
+				lspconfig.gleam.setup({})
+				lspconfig.lua_ls.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.gopls.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.cssls.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.rust_analyzer.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.ts_ls.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.html.setup({
+					capabilities = capabilities,
+				})
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			end,
+		},
 
-        {
-            "neovim/nvim-lspconfig",
-            config = function()
-                local capabilities = require("cmp_nvim_lsp").default_capabilities()
-                local lspconfig = require("lspconfig")
-                lspconfig.gleam.setup({})
-                lspconfig.lua_ls.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.gopls.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.cssls.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.rust_analyzer.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.ts_ls.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.html.setup({
-                    capabilities = capabilities,
-                })
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-                vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-            end,
-        },
+		{
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		{
+			"L3MON4D3/LuaSnip",
+			dependencies = {
+				"saadparwaiz1/cmp_luasnip",
+				"rafamadriz/friendly-snippets",
+			},
+		},
 
-        {
-            "nvimtools/none-ls.nvim",
-            config = function()
-                local null_ls = require("null-ls")
-                null_ls.setup({
-                    sources = {
-                        null_ls.builtins.formatting.stylua,
-                        null_ls.builtins.formatting.gofumpt,
-                        null_ls.builtins.formatting.golines,
-                        null_ls.builtins.formatting.goimports,
-                    },
-                })
+		{
+			"hrsh7th/nvim-cmp",
+			config = function()
+				local cmp = require("cmp")
+				require("luasnip.loaders.from_vscode").lazy_load()
+				cmp.setup({
+					snippet = {
+						-- REQUIRED - you must specify a snippet engine
+						expand = function(args)
+							--vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+							require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+						end,
+					},
+					window = {
+						--completion = cmp.config.window.bordered(),
+						documentation = cmp.config.window.bordered(),
+					},
+					mapping = cmp.mapping.preset.insert({
+						["<C-b>"] = cmp.mapping.scroll_docs(-4),
+						["<C-f>"] = cmp.mapping.scroll_docs(4),
+						["<C-Space>"] = cmp.mapping.complete(),
+						["<C-e>"] = cmp.mapping.abort(),
+						["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					}),
+					sources = cmp.config.sources({
+						{ name = "nvim_lsp" },
+						{ name = "luasnip" }, -- For luasnip users.
+					}, {
+						{ name = "buffer" },
+					}),
+				})
+			end,
+		},
+		-- end
+		{
+			"nvim-tree/nvim-web-devicons",
+		},
+		-- [[
+		-------------------------------------------------
+		--| A | B | C                             X | Y | Z |
+		------------------------------------------------- ]]
+		{
+			"nvim-lualine/lualine.nvim",
+			enabled = true,
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			config = function()
+				require("lualine").setup({
+					icons_enabled = true,
+					theme = "auto",
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+					disabled_filetypes = {
+						statusline = {},
+						winbar = {},
+					},
+				})
+			end,
+		},
 
-                vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-            end,
-        },
-        {
-            "hrsh7th/cmp-nvim-lsp",
-        },
-        {
-            "L3MON4D3/LuaSnip",
-            dependencies = {
-                "saadparwaiz1/cmp_luasnip",
-                "rafamadriz/friendly-snippets",
-            },
-        },
+		-- best plugin
+		{
+			"ThePrimeagen/harpoon",
+		},
+	},
 
-        {
-            "hrsh7th/nvim-cmp",
-            config = function()
-                local cmp = require("cmp")
-                require("luasnip.loaders.from_vscode").lazy_load()
-                cmp.setup({
-                    snippet = {
-                        -- REQUIRED - you must specify a snippet engine
-                        expand = function(args)
-                            --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-                        end,
-                    },
-                    window = {
-                        --completion = cmp.config.window.bordered(),
-                        documentation = cmp.config.window.bordered(),
-                    },
-                    mapping = cmp.mapping.preset.insert({
-                        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                        ["<C-Space>"] = cmp.mapping.complete(),
-                        ["<C-e>"] = cmp.mapping.abort(),
-                        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                    }),
-                    sources = cmp.config.sources({
-                        { name = "nvim_lsp" },
-                        { name = "luasnip" }, -- For luasnip users.
-                    }, {
-                        { name = "buffer" },
-                    }),
-                })
-            end,
-        },
-        -- end
-        {
-            "nvim-tree/nvim-web-devicons",
-        },
-        -- [[
-        -------------------------------------------------
-        --| A | B | C                             X | Y | Z |
-        ------------------------------------------------- ]]
-        {
-            "nvim-lualine/lualine.nvim",
-            enabled = true,
-            dependencies = { "nvim-tree/nvim-web-devicons" },
-            config = function()
-                require("lualine").setup({
-                    icons_enabled = true,
-                    theme = "auto",
-                    component_separators = { left = "", right = "" },
-                    section_separators = { left = "", right = "" },
-                    disabled_filetypes = {
-                        statusline = {},
-                        winbar = {},
-                    },
-                })
-            end,
-        },
-
-        -- best plugin
-        {
-            "ThePrimeagen/harpoon",
-        },
-    },
-
-    install = { colorscheme = { "habamax" } },
-    checker = { enabled = false },
+	install = { colorscheme = { "habamax" } },
+	checker = { enabled = false },
 })
 local config = require("nvim-treesitter.configs")
 config.setup({
-    auto_install = true,
-    highlight = { enable = true },
-    indent = { enable = true },
+	auto_install = true,
+	highlight = { enable = true },
+	indent = { enable = true },
 })
+
+-- Options / Binds
+
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
 vim.cmd("au ColorScheme * hi Comment cterm=italic gui=italic")
--- Options / Binds
 vim.keymap.set("n", "<leader>e", ':lua require("harpoon.ui").toggle_quick_menu()<CR>')
 vim.keymap.set("n", "<a-h>", ':lua require("harpoon.mark").add_file()<CR>')
 vim.keymap.set("n", "<C-p>", ":Telescope harpoon marks<CR>")
@@ -222,7 +206,6 @@ vim.o.undodir = vim.fn.expand("~/.local/share/nvim/undo")
 vim.o.undofile = true
 vim.cmd("hi Normal ctermbg=none guibg=none")
 vim.cmd.colorscheme("zenbones")
-require("colorizer").setup()
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
 require("leap").create_default_mappings()
 
@@ -233,5 +216,5 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set nohlsearch")
 vim.opt.clipboard = "unnamedplus"
-
-vim.cmd("set number") 
+vim.cmd("set number")
+vim.keymap.set("n", "<leader>gf", "ggVG=", { noremap = true, silent = true })
